a07c250ec058ec66b365905dcaa76cfc3c05681b
Azhar Lateef <azhar.lateef.9@gmail.com>
2025-03-19 13:12:31 +0500

    made components

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import CustomerCarousel from "@/components/ui/Carousel";
+import CoffeSolution from "@/components/ui/CoffeSolution";
 
 export default function Home() {
   return (
@@ -121,34 +122,7 @@ export default function Home() {
         </div>
         {/* ==Coffee Solution for== */}
 
-        <div className="w-full bg-themeLight">
-          <h4 className="text-7xl text-white text-center font-robotoSerif font-bold mx-auto leading-[125px] mb-10">
-            Coffee Solution for
-          </h4>
-
-          <div className="w-full flex">
-            <div className="">
-              <img src="/images/bakeries.png" />
-            </div>
-            <div className="">
-              <img src="/images/cafeterias.png" />
-            </div>
-            <div className="">
-              <img src="/images/cafes.png" />
-            </div>
-            <div className="">
-              <img src="/images/cstores.png" />
-            </div>
-          </div>
-          <div className="w-full flex justify-center">
-            <div className="">
-              <img src="/images/restaurants.png" />
-            </div>
-            <div className="">
-              <img src="/images/hospitality.png" />
-            </div>
-          </div>
-        </div>
+        <CoffeSolution/>
 
         {/* ==Our mission== */}
 
