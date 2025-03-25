96711e9c6ce45e0a8e839174274e5aa3452a3e3e
hamzasaed7568 <hamzasaeed75685962@gmail.com>
2025-03-25 16:01:50 +0500

    loader added & email templates work done

--- src/app/(auth)/sign-in/page.js
+++ src/app/(auth)/sign-in/page.js
@@ -1,4 +1,5 @@
 "use client";
+import MiniLoader from "@/components/ui/MiniLoader";
 import { loginSchema } from "@/schema";
 import { loginAPI } from "@/utilities/PostAPI";
 import { error_toaster, success_toaster } from "@/utilities/Toaster";
@@ -6,9 +7,11 @@ import { useFormik } from "formik";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Checkbox } from "primereact/checkbox";
+import { useState } from "react";
 
 export default function SignIn() {
   const router = useRouter();
+  const [loader, setLoader] = useState(false);
   const initialValues = {
     email: "",
     password: "",
@@ -18,12 +21,14 @@ export default function SignIn() {
       initialValues,
       validationSchema: loginSchema,
       onSubmit: async (values, action) => {
+        setLoader(true);
         let res = await loginAPI("api/v1/users/login", {
           email: values.email,
           password: values.password,
         });
         console.log("🚀 ~ onSubmit: ~ res:", res?.data);
         if (res?.data?.status === "success") {
+          setLoader(false);
           router.push("/");
           localStorage.setItem("accessToken", res?.data?.data?.token);
           localStorage.setItem("loginStatus", true);
@@ -33,6 +38,7 @@ export default function SignIn() {
           // setLoginStatus(true);
           success_toaster("Login Successfully");
         } else if (res?.data?.status === "error") {
+          setLoader(false);
           error_toaster(res?.data?.message);
         }
         action.resetForm();
@@ -59,70 +65,76 @@ export default function SignIn() {
             Sign In to Busy Bean
           </h1>
 
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="flex flex-col gap-y-4">
-              <div className="flex flex-col gap-y-2">
-                <label className="text-white font-medium font-satoshi">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  placeholder="Email"
-                  className="border border-inputBorder rounded-lg outline-none px-3 py-2"
-                />
-                <div className={errors.email && touched.email}>
-                  {errors.email && touched.email && (
-                    <div className=" text-red-600 space-y-1 pb-1">
-                      <hr className="border-none h-0.5 bg-white bg-opacity-20" />
-                      <p>{errors.email}</p>
-                    </div>
-                  )}
+          {loader ? (
+            <MiniLoader />
+          ) : (
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div className="flex flex-col gap-y-4">
+                <div className="flex flex-col gap-y-2">
+                  <label className="text-white font-medium font-satoshi">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    placeholder="Email"
+                    className="border border-inputBorder rounded-lg outline-none px-3 py-2"
+                  />
+                  <div className={errors.email && touched.email}>
+                    {errors.email && touched.email && (
+                      <div className=" text-red-600 space-y-1 pb-1">
+                        <hr className="border-none h-0.5 bg-white bg-opacity-20" />
+                        <p>{errors.email}</p>
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="flex flex-col gap-y-2">
+                  <label className="text-white font-medium font-satoshi">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    placeholder="password"
+                    className="border border-inputBorder rounded-lg outline-none px-3 py-2"
+                  />
+                  <div className={errors.password && touched.password}>
+                    {" "}
+                    {errors.password && touched.password && (
+                      <div className="text-red-600 space-y-1 pb-1">
+                        <hr className="border-none h-0.5 bg-white bg-opacity-20" />
+                        <p>{errors.password}</p>
+                      </div>
+                    )}
+                  </div>
+                  <p className="text-white text-sm text-end font-normal">
+                    Forgot Password?
+                  </p>
                 </div>
               </div>
-              <div className="flex flex-col gap-y-2">
-                <label className="text-white font-medium font-satoshi">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  placeholder="password"
-                  className="border border-inputBorder rounded-lg outline-none px-3 py-2"
-                />
-                <div className={errors.password && touched.password}>
-                  {" "}
-                  {errors.password && touched.password && (
-                    <div className="text-red-600 space-y-1 pb-1">
-                      <hr className="border-none h-0.5 bg-white bg-opacity-20" />
-                      <p>{errors.password}</p>
-                    </div>
-                  )}
-                </div>
-                <p className="text-white text-sm text-end font-normal">
-                  Forgot Password?
-                </p>
+              <div>
+                <button
+                  type="submit"
+                  className="bg-theme font-satoshi text-white py-2 rounded-lg w-full font-medium outline-none "
+                >
+                  Sign In
+                </button>
               </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="bg-theme font-satoshi text-white py-2 rounded-lg w-full font-medium outline-none "
-              >
-                Sign In
-              </button>
-            </div>
-            <p className="font-switzer">
-              <span className="text-opacity-70 text-white">New Customer?</span>{" "}
-              <Link href={"/sign-up"}>
-                <u className="text-white">Signup</u>
-              </Link>
-            </p>
-          </form>
+              <p className="font-switzer">
+                <span className="text-opacity-70 text-white">
+                  New Customer?
+                </span>{" "}
+                <Link href={"/sign-up"}>
+                  <u className="text-white">Signup</u>
+                </Link>
+              </p>
+            </form>
+          )}
         </div>
       </div>
     </div>
---
+++ src/components/ui/Loader.jsx
@@ -0,0 +1,20 @@
+import { CirclesWithBar } from "react-loader-spinner";
+
+export default function Loader() {
+  return (
+    <div className="bg-theme w-full h-screen flex items-center justify-center">
+      <CirclesWithBar
+        height="120"
+        width="120"
+        color="#FFFFFF"
+        outerCircleColor="#FFFFFF"
+        innerCircleColor="#FFFFFF"
+        barColor="#FFFFFF"
+        ariaLabel="circles-with-bar-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+}
