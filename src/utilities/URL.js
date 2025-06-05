7243a6b9a1cf73a6e19f2e611a9284e7c33fea1b
hamzasaed7568 <hamzasaeed75685962@gmail.com>
2025-06-05 17:53:46 +0500

    incpice suuccess and failure apges added

--- src/utilities/URL.js
+++ src/utilities/URL.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "http://192.168.1.107:8011/";
+// export const BASE_URL = "http://192.168.1.107:8011/";
 
 export const BASE_URL_SOCKET = "http://bb.allfourcar.com:8011";
 
 export const googleApiKey = "AIzaSyD68_vw1gGE7LVVjJ5ZShy7qWwm9Rq0CBQ";
 
-// export const BASE_URL = "https://backendbb.trimworldwide.com/" 
+export const BASE_URL = "https://backendbb.trimworldwide.com/" 
